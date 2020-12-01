49bb3ba762db96acfa7c13a670d080192254f9cd
Are <aregulliksen+github@gmail.com>
2020-12-01 12:05:16 +0100

    Satt opp server og client
    Lite problem på server-delen

---
+++ kode/server/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import {PORT} from './constants/index.js';
+import 'dotenv/config.js';
+import errorMiddleware from './middleware/errors.js';
+import connectDatabase from './config/db.js';
+
+const app = express();
+
+//Legge til production?
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    allowedHeaders: ['Content-Type']
+  }))
+
+  //app.use(`${process.env.BASEURL}/XXXXX`, XXXXX);
+  //app.use(`${process.env.BASEURL}/XXXXX`, XXXXX);
+  //app.use(`${process.env.BASEURL}/XXXXX`, XXXXX);
+  //app.use(`${process.env.BASEURL}/XXXXX`, XXXXX);
+
+  app.use(errorMiddleware);
+
+  connectDatabase;
+
+  const server = app.listen(
+      PORT,
+      console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  );
+
+  process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    });
+});
