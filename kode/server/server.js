dcb3302d5406295ad5344c50e988e82b386899b6
elisedm <elise.d.mellegard@hiof.no>
2020-12-08 12:45:03 +0100

    Lagt på sikkerhetstiltak som vist i leksjon 13_08

--- kode/server/server.js
+++ kode/server/server.js
@@ -3,8 +3,17 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import xssClean from 'xss-clean';
+import csrf from 'csurf';
+import mongoSantitize from 'express-mongo-sanitize';
+import rateLimit from 'express-rate-limit';
+
 import {PORT} from './constants/index.js';
 import 'dotenv/config.js';
+
+
 import errorMiddleware from './middleware/errors.js';
 import connectDatabase from './config/db.js';
 import article from './routes/article.js';
@@ -17,6 +26,19 @@ import email from './routes/email.js';
 
 
 const app = express();
+// Sikkerhetstiltak er satt opp fra leksjon 13_08
+app.use(helmet());
+app.use(mongoSantitize());
+app.use(xssClean());
+app.use(hpp());
+
+const limiter = rateLimit({
+    windowMs: 10*60*1000,
+    max: 100,
+});
+
+app.use(limiter);
+
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -27,11 +49,17 @@ app.use(express.static(`./public`))
 
 app.use(cors({
     origin: 'http://localhost:3000',
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
     credentials: true,
   }))
 
   app.use(cookieParser());
+  //Hentet fra leksjon 13_08
+  app.use(csrf({cookie: true}));
+
+  app.get(`${process.env.BASEURL}/csrf-token`, (req, res) => {
+      res.status(200).json({data: req.csrfToken() });
+  });
 
   
  
